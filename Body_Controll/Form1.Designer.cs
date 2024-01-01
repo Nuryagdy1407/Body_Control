262672fd0952f01c9686ef9a2dabed206413886c
Nuryagdy Azadov <74148048+Nuryagdy1407@users.noreply.github.com>
2024-01-01 14:16:42 +0500

    Source C

---
+++ Body_Controll/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Body_Controll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblBoy = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(150, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(792, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Boý we agram gatnaşygynyň awtomatlaşdyrylan ulgamy";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(458, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "HASAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Century", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(36, 521);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1146, 120);
+            this.label6.TabIndex = 4;
+            this.label6.Text = " Siziň boý we agram gatnaşygyňyz kadaly ýagdaýda!";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Body_Controll.Properties.Resources.baydak_vector;
+            this.pictureBox2.Location = new System.Drawing.Point(1026, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 139);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Body_Controll.Properties.Resources._6_230x28_kопировать;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(183, 50);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(814, 136);
+            this.textBox3.TabIndex = 124;
+            this.textBox3.Text = "\r\nTürkmenistanyň Goranmak Ministrliginiň Berdimuhamet Annaýew adyndaky 1-nji Ýöri" +
+    "teleşdirilen harby mekdebi\r\n";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(417, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 125;
+            this.label7.Text = " ";
+            this.label7.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBaglan);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.cmbPorts);
+            this.panel1.Location = new System.Drawing.Point(735, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 139);
+            this.panel1.TabIndex = 126;
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.BackColor = System.Drawing.Color.Green;
+            this.btnBaglan.Enabled = false;
+            this.btnBaglan.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBaglan.Location = new System.Drawing.Point(213, 42);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(148, 34);
+            this.btnBaglan.TabIndex = 127;
+            this.btnBaglan.Text = "baglandy";
+            this.btnBaglan.UseVisualStyleBackColor = false;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(24, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 25);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Port:";
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.Enabled = false;
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(86, 49);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.lblBoy);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(41, 108);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(457, 118);
+            this.panel2.TabIndex = 127;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(251, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 132;
+            this.label8.Text = " ";
+            this.label8.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(122, 63);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 36);
+            this.textBox2.TabIndex = 131;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(200, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 25);
+            this.label5.TabIndex = 126;
+            this.label5.Text = "kg";
+            // 
+            // lblBoy
+            // 
+            this.lblBoy.AutoSize = true;
+            this.lblBoy.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBoy.Location = new System.Drawing.Point(117, 20);
+            this.lblBoy.Name = "lblBoy";
+            this.lblBoy.Size = new System.Drawing.Size(24, 25);
+            this.lblBoy.TabIndex = 127;
+            this.lblBoy.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(200, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 25);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "sm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(32, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 129;
+            this.label4.Text = "Agram:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 130;
+            this.label2.Text = "Boý:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(37, 208);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1140, 311);
+            this.panel3.TabIndex = 128;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1198, 660);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblBoy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+
+
+    }
+}
+
---
+++ Body_Controll/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Body_Controll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox2.Enabled = true;
+            label6.Visible = true;
+            label6.Text = "Haýyş edýän enjama baglanyň!";
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            { cmbPorts.Items.Add(port); }
+            if (cmbPorts.Items.Count > 0) { cmbPorts.SelectedIndex = 0; }
+            AutoPortDetector();
+            timer1.Start();
+            timer1.Interval = 3000;
+        }
+
+        public void AutoPortDetector()
+        {
+            serialPort1.Close();
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                serialPort1.PortName = port;
+
+            }
+            try
+            {
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Maglumat beriji enjamlar bilen baglanyşyk kesilen! Baglanyşygy barlaň!");
+                Form1_Load(MessageBoxButtons.OKCancel, null);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hasapla();
+        }
+
+        private void hasapla()
+        {
+            label6.Visible = false;
+            int boy = System.Convert.ToInt32(lblBoy.Text);
+            int agram = System.Convert.ToInt32(textBox2.Text);
+            double weight = Convert.ToDouble(agram);
+            double height = Convert.ToDouble(boy);
+            double netije = weight/(Math.Pow((height/100),2));
+
+            if (netije >= 25)
+            {
+                label6.Text = "Siziň boý we agram gatnaşygyňyz kadaly ýagdaýda dal! Size köpüräk sport bilen meşgullanmak maslahat berilýär!!";
+                label6.Visible = true;
+            }
+            else if(netije > 18.5)
+            {
+                label6.Text = "Siziň boý we agram gatnaşygyňyz kadaly ýagdaýda! Berekella!";
+                label6.ForeColor = Color.Green;
+                label6.Visible = true;
+            }
+            else
+            {
+                label6.Visible = true;
+                label6.ForeColor = Color.Red;
+                label6.Text = "Siziň boý we agram gatnaşygyňyz kadaly ýagdaýda dal! Size köpüräk iýmek maslahat berilýär!";
+            }
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+
+            AutoPortDetector();
+            if (serialPort1.IsOpen == true)
+            {
+                btnBaglan.BackColor = Color.Green;
+                btnBaglan.Text = "Baglandy";
+                btnBaglan.Enabled = false;
+                label6.Visible = false;
+                textBox2.Enabled = true;
+                string boy = serialPort1.ReadLine();
+                Console.WriteLine(boy);
+                //timer1.Start();
+            }
+            else
+                MessageBox.Show("Enjam baglanmady!");
+        }
+
+        private void baglan()
+        {
+            if (cmbPorts.Items.Count > 0)
+            {
+                serialPort1 = new System.IO.Ports.SerialPort();
+                serialPort1.PortName = cmbPorts.SelectedItem.ToString();
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch { }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string boy = serialPort1.ReadLine();
+            //Console.WriteLine(boy);
+            //int boy = Convert.ToInt32(boy);
+            lblBoy.Text = boy.ToString();
+            if (lblBoy.Text != "" & textBox2.Text != "")
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+                label6.Text = "Boýuňyzy we agramyňyzy giriziň!";
+                label6.Visible = true;
+            }
+        }
+    }
+}
